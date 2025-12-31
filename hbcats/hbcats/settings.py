0ef684371237b40ed96d30b548f1ad452fed2abf
Tim Arbuckle <timarbuckle@gmail.com>
2025-12-31 11:42:13 -0800

    get working when DEBUG=False

--- hbcats/hbcats/settings.py
+++ hbcats/hbcats/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0=98+^cir)1p_hq7^$&v-q8*ygi7mlbx3#)=$=_&qxhn9di*r8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
