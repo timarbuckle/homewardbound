35cced0ca437ad73a1e0f2dd6b36079f1dc3c7b0
Tim Arbuckle <timarbuckle@gmail.com>
2026-01-06 10:54:57 -0800

    add script to update tailwindcss for mac and linux

--- tw.sh
+++ tw.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+OS=$(uname -s)
+if [[ "$OS" == "Linux" ]]; then
+  tw="./bin/tailwindcss-linux-arm64"
+elif [[ "$OS" == "Darwin" ]]; then
+  tw="./bin/tailwindcss-macos-arm64"
+else
+  echo "Unsupported OS: $OS"
+  exit 1
+fi
+
 # update tailwind css
-./tailwindcss -i hbcats/cats/static/cats/css/input.css -o hbcats/static/cats/css/output.cs
+"$tw" -i hbcats/cats/static/cats/css/input.css -o hbcats/static/cats/css/output.cs
