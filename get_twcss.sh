c3ac9323dc76cb851d119b034fd0c40231c2d588
Tim Arbuckle <timarbuckle@gmail.com>
2026-01-07 14:26:39 -0800

    fix typo, only download tailwind binary for current os

--- get_twcss.sh
+++ get_twcss.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+OS=$(uname -s)
+if [[ "$OS" == "Linux" ]]; then
+  tw="tailwindcss-linux-arm64"
+elif [[ "$OS" == "Darwin" ]]; then
+  tw="tailwindcss-macos-arm64"
+else
+  echo "OS not supported: $OS"
+  exit 1
+fi
 echo "Pulling tailwindcss binaries"
-wget -P ./bin https://github.com/tailwindlabs/tailwindcss/releases/download/v4.1.18/tailwindcss-linux-arm64
-wget -P ./bin https://github.com/tailwindlabs/tailwindcss/releases/download/v4.1.18/tailwindcss-macos-arm64
+wget -P ./bin "https://github.com/tailwindlabs/tailwindcss/releases/download/v4.1.18/$tw"
+chmod +x "./bin/$tw"
