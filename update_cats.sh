eb2d35eb7b7b50df2880b381b5e43360b0a0993a
tim arbuckle <timarbuckle@gmail.com>
2026-01-19 18:41:23 +0000

    don't need full path if in PATH

--- update_cats.sh
+++ update_cats.sh
@@ -3,4 +3,4 @@
 cd "$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
 PATH=/home/tim/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 export DISPLAY=:0
-/home/tim/.local/bin/uv run hbcats/manage.py update
+uv run hbcats/manage.py update
