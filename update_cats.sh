0dff74d49ff67fa663135852fdd35fcd65522c09
tim arbuckle <timarbuckle@gmail.com>
2026-01-25 23:18:05 +0000

    cannot run update with app running

--- update_cats.sh
+++ update_cats.sh
@@ -3,4 +3,6 @@
 cd "$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
 PATH=/home/tim/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 export DISPLAY=:0
+sudo systemctl stop cats
 uv run hbcats/manage.py update
+sudo systemctl start cats
