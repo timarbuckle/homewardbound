8e27769e6ae7ccd80102731cd0295aebe1fbb184
tim arbuckle <timarbuckle@gmail.com>
2026-02-03 19:46:22 +0000

    tweak to update script, latest data and images

--- update_cats.sh
+++ update_cats.sh
@@ -3,6 +3,13 @@
 cd "$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
 PATH=/home/tim/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 export DISPLAY=:0
-sudo systemctl stop cats
+echo "stopping cats service ..."
+#sudo systemctl stop cats
+sudo service cats stop
+sleep 2
+#sudo systemctl stop cats
+echo "updating cats ..."
 uv run hbcats/manage.py update
-sudo systemctl start cats
+echo "starting cats service ..."
+#sudo systemctl start cats
+sudo service cats start
