bbaa492f609c46deeeaec62d04b6fa4bb31f082f
tim arbuckle <timarbuckle@gmail.com>
2026-02-04 18:17:20 +0000

    added swap, no need to stop service, fixed path error

--- update_cats.sh
+++ update_cats.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # run job to update latest cats
 cd "$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
-PATH=/home/tim/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+PATH=/home/timarbuckle/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 export DISPLAY=:0
-echo "stopping cats service ..."
+#echo "stopping cats service ..."
 #sudo systemctl stop cats
-sudo service cats stop
-sleep 2
+#sudo service cats stop
+#sleep 2
 #sudo systemctl stop cats
 echo "updating cats ..."
 uv run hbcats/manage.py update
 echo "starting cats service ..."
 #sudo systemctl start cats
-sudo service cats start
+#sudo service cats start
