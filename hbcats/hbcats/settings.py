9d6eb68b0c598e1e823d326eb4788f2f2501e8cd
Tim Arbuckle <timarbuckle@gmail.com>
2026-02-04 22:54:29 -0800

    convert to using postgresql

--- hbcats/hbcats/settings.py
+++ hbcats/hbcats/settings.py
@@ -83,10 +83,20 @@ WSGI_APPLICATION = "hbcats.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "hbcats",
+        "USER": "kitcat",
+        "PASSWORD": "meow",
+        "HOST": "100.84.78.98",
+        "PORT": "5432",
     }
 }
 
