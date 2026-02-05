494616ebbe241363fac428db53bee535b31a82db
Tim Arbuckle <timarbuckle@gmail.com>
2026-02-04 20:14:15 -0800

    update_cats.sh now to run locally, ignore .DS_Store files

--- update_cats.sh
+++ update_cats.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-# run job to update latest cats
-cd "$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")"
-PATH=/home/timarbuckle/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
-export DISPLAY=:0
-#echo "stopping cats service ..."
-#sudo systemctl stop cats
-#sudo service cats stop
-#sleep 2
-#sudo systemctl stop cats
+
+# this script is now for running manually instead of via cron
 echo "updating cats ..."
+#systemctl --user start cats.service
 uv run hbcats/manage.py update
 echo "starting cats service ..."
-#sudo systemctl start cats
-#sudo service cats start
